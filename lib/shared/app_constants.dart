d2ab03fd3f0840568289fefac3f16a6a2603ae25
Carlos Eduardo <carlos_eduardo_a.r@outlook.com>
2025-08-12 20:53:31 -0300

    feat: inicio da implementacao da autenticacao e quase conslusao

--- lib/shared/app_constants.dart
+++ lib/shared/app_constants.dart
@@ -1,9 +1,18 @@
 class AppConstants {
-  // Informações do App
+  // Textos
   static const String appName = 'MultiApp';
   static const String appSlogan = 'Seu painel pessoal inteligente';
   static const String appLoginMsg = 'Entre com sua conta para continuar';
   static const String appLoginUserInput = 'Usuário';
   static const String appLoginUserPassword = 'Senha';
   static const String appLoginUserEntry = 'Entrar';
+  static const String appLoginTypeYourUser = 'Digite seu usuário';
+  static const String appLoginFillUserField = 'Preencha o campo usuário';
+  static const String appLoginTypeYourPassword = 'Digite sua senha';
+  static const String appLoginFillPasswordField = 'Preencha o campo senha';
+  static const String appLoginWrongCredentialsMessage =
+      'As credenciais informadas estão incorretas';
+
+  // Informações de API
+  static const String baseAuthApiUrl = 'https://dummyjson.com/';
 }
--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:multi_app/components/app_button.dart';
+import 'package:multi_app/components/custom_snack_bar.dart';
 import 'package:multi_app/components/custom_text_field.dart';
+import 'package:multi_app/controllers/auth_controller.dart';
 import 'package:multi_app/shared/app_constants.dart';
 
 class LoginPage extends StatefulWidget {
@@ -14,6 +16,7 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _obscureText = true;
 
   @override
   void dispose() {
@@ -22,23 +25,80 @@ class _LoginPageState extends State<LoginPage> {
     super.dispose();
   }
 
+  Future<void> _login() async {
+    if (_formKey.currentState!.validate()) {
+      bool login = await AuthController.instance.login(
+        _usernameController.text,
+        _passwordController.text,
+      );
+      if (login) {
+        //Navegação
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          customSnackBar(
+            message: AppConstants.appLoginWrongCredentialsMessage,
+            backgroundColor: Color(0xffff6b6b),
+            icon: Icons.error_outline,
+          ),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(24.0),
         child: Form(
+          key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               Text(AppConstants.appLoginMsg, textAlign: TextAlign.center),
               const SizedBox(height: 32.0),
-              CustomTextField(label: AppConstants.appLoginUserInput),
+              CustomTextField(
+                label: AppConstants.appLoginUserInput,
+                hint: AppConstants.appLoginTypeYourUser,
+                controller: _usernameController,
+                prefixIcon: Icon(Icons.person),
+                validator: (username) {
+                  if (username == null || username.isEmpty) {
+                    return AppConstants.appLoginFillUserField;
+                  }
+                  return null;
+                },
+              ),
               const SizedBox(height: 16.0),
-              CustomTextField(label: AppConstants.appLoginUserPassword),
+              CustomTextField(
+                label: AppConstants.appLoginUserPassword,
+                hint: AppConstants.appLoginTypeYourPassword,
+                controller: _passwordController,
+                prefixIcon: Icon(Icons.lock_outline),
+                obscureText: _obscureText,
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _obscureText = !_obscureText;
+                    });
+                  },
+                  icon: Icon(
+                    _obscureText ? Icons.visibility_off : Icons.visibility,
+                  ),
+                ),
+                validator: (password) {
+                  if (password == null || password.isEmpty) {
+                    return AppConstants.appLoginFillPasswordField;
+                  }
+                  return null;
+                },
+              ),
               const SizedBox(height: 16.0),
-              AppButton(text: AppConstants.appLoginUserEntry, onPressed: () {}),
+              AppButton(
+                text: AppConstants.appLoginUserEntry,
+                onPressed: _login),
+              ),
             ],
           ),
         ),
